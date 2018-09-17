b4d2ff5f517fdd6b82aaf94dab45cb644f09583e
Hibatallah AOUADNI <hibatallah.aouadni@sifast.com>
2018-09-17 17:35:01 +0100

    FO: add a basic list

--- src/pages/appareils/appareils.ts
+++ src/pages/appareils/appareils.ts
@@ -7,6 +7,19 @@ import { SingleAppareilPage } from '../single-appareil/single-appareil';
   templateUrl: 'appareils.html'
 })
 export class AppareilsPage {
+
+    appareilsList = [
+        {
+          name: 'Machine à laver'
+        },
+        {
+          name: 'Télévision'
+        },
+        {
+          name: 'Ordinateur'
+        }
+      ];
+      
   constructor(private navCtrl: NavController) {}
 
   onLoadAppareil(name: string) {
